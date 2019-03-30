c2084d5b5462bdb47ae229c5cd30e9460c55a30b
Ken Bowen <ken.bowen@outlook.com>
2019-03-30 17:07:31 -0500

    Used Property Injection

--- src/main/java/com/y2kbowen/service/CustomerServiceImpl.java
+++ src/main/java/com/y2kbowen/service/CustomerServiceImpl.java
@@ -2,13 +2,18 @@ package com.y2kbowen.service;
 
 import com.y2kbowen.model.Customer;
 import com.y2kbowen.repository.CustomerRepository;
-import com.y2kbowen.repository.HibernateCustomerRepositoryImpl;
 
 import java.util.List;
 
 public class CustomerServiceImpl implements CustomerService {
 
-    private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
+
+    private CustomerRepository customerRepository;
+
+    public void setCustomerRepository(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
 
     @Override
     public List<Customer> findAll() {
