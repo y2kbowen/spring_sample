ad48a60eb025ef0ddecd3a86efe2d2f4e942f319
Ken Bowen <ken.bowen@outlook.com>
2019-03-30 19:25:46 -0500

    Learned annotation style

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,11 +1,15 @@
 import com.y2kbowen.service.CustomerService;
 import com.y2kbowen.service.CustomerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
 
     public static void main(String[] args) {
-        CustomerService service = new CustomerServiceImpl();
 
+        ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        CustomerService service = appContext.getBean("customerService",CustomerService.class);
 
         System.out.println(service.findAll().get(0).getFirstname());
 
